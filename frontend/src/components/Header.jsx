1b1b5df84efe5db7c7c5b7d1dfb96e46ecd95d03
Thein Naing <theinnaing0908@gmail.com>
2023-06-02 20:55:40 +0800

    create Header.js in components folder and add coding for Navbar.

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,33 @@
+import{ Navbar, Nav, Container} from 'react-bootstrap';
+import{ LinkContainer} from 'react-router-bootstrap';
+import { FaShoppingCart, FaUser} from 'react-icons/fa';
+
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar
+      bg='dark'
+      variant='dark'
+      expand= 'lg'
+      collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">
+            ProShop
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+            <Nav.Link href='/cart'><FaShoppingCart />Cart</Nav.Link>
+            <Nav.Link href='/login'><FaUser />Sign In</Nav.Link>
+            </Nav>
+
+          </Navbar.Collapse>
+        </Container>
+
+      </Navbar>
+    </header>
+  )
+}
+
+export default Header
