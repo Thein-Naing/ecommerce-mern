0b23ca4239629768f19362b51a2dec506e164ed2
Thein Naing <theinnaing0908@gmail.com>
2023-06-02 21:14:08 +0800

    Finished coding for Footer.js. Import Footer.js into index.js and  add as component.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
     <main className='py-3'>
       <Container>
     <h1>Welcome to Proshop</h1>
+    <Footer />
     </Container>
     </main>
     </>
