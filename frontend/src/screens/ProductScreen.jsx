032f51cd2d3adf333aa12693a0acece46bde8787
Thein Naing <theinnaing0908@gmail.com>
2023-06-03 15:17:40 +0800

    update ProduceScree.js with price/description tag and also add a button to add item to cart.

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import { Row, Col, Image, ListGroup, Card, Button, ListGroupItem } from 'react-bootstrap';
 import products from '../products';
 import Rating from '../components/Rating';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
@@ -21,6 +21,7 @@ const ProductScreen = () => {
       <Col md={5}>
         <Image src={product.image} alt={product.name} fluid/>
       </Col>
+
       <Col md={4}>
         <ListGroup variant='flush'>
           <ListGroup.Item>
@@ -30,20 +31,54 @@ const ProductScreen = () => {
             <Rating  value={product.rating} text={`${product.numReviews} reviews`}/>
           </ListGroup.Item>
           <ListGroup.Item>
-            price:  ${product.price}
+            Price:  ${product.price}
+          </ListGroup.Item>
+          <ListGroup.Item>
+            Description: {product.description}
           </ListGroup.Item>
         </ListGroup>
       </Col>
+
       <Col md={3}>
         <Card>
         <ListGroup variant='flush'>
+        <ListGroup.Item>
+          <Row>
+            <Col>Price:</Col>
+            <Col>
+            <strong>${product.price}</strong>
+            </Col>
+          </Row>
+          </ListGroup.Item>
           </ListGroup>
+
+          <ListGroup variant='flush'>
+        <ListGroup.Item>
+          <Row>
+            <Col>Status:</Col>
+            <Col>
+            <strong>{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</strong>
+            </Col>
+          </Row>
+          </ListGroup.Item>
+          </ListGroup>
+          <ListGroup.Item>
+            <Button
+            className='btn btn-block'
+            type='button'
+            disabled ='{product.countInStock === 0}'> Add to cart
+            </Button>
+          </ListGroup.Item>
+
+
+
+
+
+
 
         </Card>
       </Col>
     </Row>
-
-
     </>
   )
 }
