874368ab95153d9937530f1625a0b95d4bb069cc
Thein Naing <theinnaing0908@gmail.com>
2023-06-03 12:42:13 +0800

    create ProductScreen.jsx for individual product viewing, import it to index.js and render as component with Route element.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,13 +7,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {createBrowserRouter, createRoutesFromElements,
   Route, RouterProvider} from 'react-router-dom';
-  import HomeScreen from './screens/HomeScreen';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element={<App />}>
       <Route index={true} path='/' element={<HomeScreen />}/>
+      <Route path='/product/:id' element={<ProductScreen />}/>
       </Route>
 
     )
