8f8982d9b5e26d886bef9d57f8d725f52275fbf8
Thein Naing <theinnaing0908@gmail.com>
2023-06-03 11:38:22 +0800

    get rid of <HomeScreen /> component from App.js since we already import in index.js.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import HomeScreen from './screens/HomeScreen';
+
 
 
 const App = () => {
@@ -10,7 +10,7 @@ const App = () => {
     <Header />
     <main className='py-3'>
       <Container>
-      <HomeScreen />
+
     <Footer />
     </Container>
     </main>
