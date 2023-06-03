5f3196a11a6242db3363ff208cf1fa17c1d97b66
Thein Naing <theinnaing0908@gmail.com>
2023-06-03 12:46:31 +0800

    import {Link } from 'react-router-dom', import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap', import Rating from '../components/Rating' into ProductScreen.jsx to make rendering view product rendering.

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,13 +1,18 @@
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import products from '../products';
+import Rating from '../components/Rating';
 
 const ProductScreen = () => {
   const { id: productId} = useParams();
   const product = products.find((p) => p._id === productId);
-  console.log(product);
+
 
   return (
-    <div>ProductScreen</div>
+    <>
+
+
+    </>
   )
 }
 
