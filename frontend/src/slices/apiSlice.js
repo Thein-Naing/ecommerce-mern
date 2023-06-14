2307fa88ef664e6c7951b2cdc1b7b4643f56db0e
Thein Naing <theinnaing0908@gmail.com>
2023-06-14 09:45:53 +0800

    create parent apiSLice in apiSLice.js like below: import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'; import { BASE_URL} from '../constants';
    const baseQuery = fetchBaseQuery({baseURL: BASE_URL});
    
    // parent of all apiSLices.
    export const apiSlice = createApi({
      baseQuery,
      tagTypes: ['Products', 'Order', 'User'],
      endpoints: (builder) => ({}),
    });
    . We also set up endpoints for all api requests here so that we can make fetching more clearly.

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,2 +1,11 @@
 import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 import { BASE_URL} from '../constants';
+
+const baseQuery = fetchBaseQuery({baseURL: BASE_URL});
+
+// parent of all apiSLices.
+export const apiSlice = createApi({
+  baseQuery,
+  tagTypes: ['Products', 'Order', 'User'],
+  endpoints: (builder) => ({}),
+});
