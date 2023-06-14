a13ce9a8e3a9c6eb09299ee43b3db40ee5bff63b
Thein Naing <theinnaing0908@gmail.com>
2023-06-14 09:18:55 +0800

    then export store.js to index.js and in index.js also import { Provider } from 'react-redux' to wrap app and router.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
+import { Provider } from 'react-redux';
+import store from './store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {createBrowserRouter, createRoutesFromElements,
