7925e7e492faf8c041f4ef2c44c6f8bb40ea33f0
Thein Naing <theinnaing0908@gmail.com>
2023-06-14 11:36:32 +0800

    create Message function with 2 props; import { Alert} from 'react-bootstrap';
    const Message = ({variant, children}) => { // 2 argument props.
      return (
       <Alert
       variant={variant}>
        {children}
        </Alert>
      )
    }
    
    Message.defaultProps = {
      variant: 'info'
    }
    export default Message

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,9 +1,15 @@
-import { ALert} from 'react-bootstrap';
+import { Alert} from 'react-bootstrap';
 
-const Message = () => {
+const Message = ({variant, children}) => { // 2 argument props.
   return (
-    <div>Message</div>
+   <Alert
+   variant={variant}>
+    {children}
+    </Alert>
   )
 }
 
+Message.defaultProps = {
+  variant: 'info'
+}
 export default Message
